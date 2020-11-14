d022d9ff4725d795c851218612d5917d2ca7e65c
valentinT1 <tamtam.valentin@gmail.com>
2020-11-14 13:25:19 +0100

    wall animation

---
+++ Assets/WallAssets/BrickNameScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickNameScript : MonoBehaviour
+{
+    public Transform spawnPos;
+    public GameObject spawnee;
+    public GameObject text;
+
+    public float delay = 0.5f;
+    public bool brickCanFall = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnPos.position = new Vector3(0.26f, spawnPos.position.y, spawnPos.position.z);
+        if (Input.GetKeyDown(KeyCode.Space) && brickCanFall)
+        {
+            Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+            brickCanFall = false;
+            delay = 1f;
+        }
+
+        if (delay > 0 && !brickCanFall)
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0)
+            {
+                Instantiate(text, spawnPos.position, spawnPos.rotation);
+            }
+        }
+    }
+}
---
+++ Assets/WallAssets/BrickWallConstructionScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickWallConstructionScript : MonoBehaviour
+{
+    public Transform spawnPos;
+    public GameObject spawnee;
+
+    public float delay = 0.2f;
+    public int brickCount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(delay > 0)
+        {
+            delay -= Time.deltaTime;
+            if(delay <= 0 && brickCount < 20)
+            {
+                
+                if(brickCount == 0 || brickCount == 12)
+                {
+                    spawnPos.position = new Vector3(-1f, spawnPos.position.y, spawnPos.position.z);
+                }
+                else if(brickCount == 6)
+                {
+                    spawnPos.position = new Vector3(-0.8f, spawnPos.position.y, spawnPos.position.z);
+                }
+                else
+                {
+                    spawnPos.position = new Vector3(spawnPos.position.x + 0.42f, spawnPos.position.y, spawnPos.position.z);
+                }
+
+
+                delay = 0.2f;
+                brickCount += 1;
+
+                if (brickCount != 16)
+                {
+                    Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+                }
+
+            }
+        }
+    }
+}
